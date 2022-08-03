aed320e71df4aaa71d9cdf7a1293b38af5d70c01
Warren Layson <warrenlayson@gmail.com>
2022-08-03 21:35:24 +0800

    lsp settings

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -2,6 +2,18 @@ return {
 	settings = {
 
 		Lua = {
+      format = {
+        enable = false,
+      },
+      hint = {
+        enable = true,
+        arrayIndex = "Disable", -- "Enable", "Auto", "Disable"
+        await = true,
+        paramName = "Disable", -- "All", "Literal", "Disable"
+        paramType = false,
+        semicolon = "Disable", -- "All", "SameLine", "Disable"
+        setType = true,
+      },
 			diagnostics = {
 				globals = { "vim" },
 			},
@@ -11,6 +23,9 @@ return {
 					[vim.fn.stdpath("config") .. "/lua"] = true,
 				},
 			},
+      telemetry = {
+        enable = false
+      }
 		},
 	},
 }
