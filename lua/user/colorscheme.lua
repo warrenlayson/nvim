031f4ccb544821c4fc7683cd72923c102fbbc090
Warren Layson <warrenlayson@gmail.com>
2022-08-03 17:42:17 +0800

    added colorscheme

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,12 @@
+local colorscheme = "tokyonight"
+
+-- colorscheme config
+vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
+
