44b2fc4a7f81c769faea52dda02fc5e7fdf78795
Warren Layson <warrenlayson@gmail.com>
2022-08-09 17:54:36 +0800

    added treeobjects in treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -33,4 +33,23 @@ configs.setup {
     enable = true,
     enable_autocmd = false,
   },
+  textobjects = {
+    select  = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+      -- You can choose the select mode (default is charwise 'v')
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      },
+    }
+  }
 }
