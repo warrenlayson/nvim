8752fb362537ac892d94465c6711c550a8d264b2
Warren Layson <warrenlayson@gmail.com>
2022-08-09 17:55:14 +0800

    added rules in autopairs

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -25,6 +25,15 @@ npairs.setup {
   },
 }
 
+local Rule = require "nvim-autopairs.rule"
+
+local ts_conds = require "nvim-autopairs.ts-conds"
+
+npairs.add_rules {
+  Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node { "string", "comment" }),
+  Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node { "function" }),
+}
+
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
