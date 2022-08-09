2698d076af3def918eb4164e7d4343a273937343
Warren Layson <warrenlayson@gmail.com>
2022-08-09 17:56:07 +0800

    updated functions

--- lua/user/functions.lua
+++ lua/user/functions.lua
@@ -4,6 +4,15 @@ function M.isempty(s)
   return s == nil or s == ""
 end
 
+function M.get_buf_option(opt)
+  local present, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
+  if not present then
+    return nil
+  else
+    return buf_option
+  end
+end
+
 function M.smart_quit()
   local bufnr = vim.api.nvim_get_current_buf()
   local modified = vim.api.nvim_buf_get_option(bufnr, "modified")
